1dc3caeb42d0a455a5ed47383d1119723019a824
brldevx <bruurbno@icloud.com>
2024-04-10 21:23:40 -0300

    feat: add tailwind and styles

---
+++ components/sidebar.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+import { BiSearch } from "react-icons/bi";
+import { HiHome } from "react-icons/hi";
+import Box from "@/components/box";
+import SidebarItem from "@/components/sidebar-item";
+import Library from "./library";
+
+interface SidebarProps {
+  children: React.ReactNode;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+  const pathName = usePathname();
+
+  const routes = useMemo(
+    () => [
+      {
+        icon: HiHome,
+        label: "Home",
+        active: pathName !== "/search",
+        href: "/",
+      },
+      {
+        icon: BiSearch,
+        label: "Search",
+        active: pathName === "/search",
+        href: "/search",
+      },
+    ],
+    [pathName]
+  );
+
+  return (
+    <div className="flex h-full">
+      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+        <Box className="">
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map((route) => {
+              return <SidebarItem key={route.label} {...route} />;
+            })}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">
+          <Library />
+        </Box>
+      </div>
+
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
+    </div>
+  );
+};
+
+export default Sidebar;
